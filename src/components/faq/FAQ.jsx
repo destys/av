e95a13d0e7860ac3cb87d9a3b95d96d5b51640a7
destys <desstyss@gmail.com>
2024-08-09 14:48:48 +0300

    progress

--- src/components/faq/FAQ.jsx
+++ src/components/faq/FAQ.jsx
@@ -1,24 +1,39 @@
+"use client";
 import { replaceVariablesInText } from "@/utils/extractDataFromParams";
 import FAQItem from "./faq-item/FaqItem";
 
 import styles from "./FAQ.module.scss";
+import { useState } from "react";
+import Button from "../ui/button/Button";
 
 export default function FAQ({ data, params }) {
   if (!data) return null;
 
   const faqs = data.attributes.faqs;
+  const [faqsList, setFaqsList] = useState(faqs.slice(0, 5));
 
   return (
     <section className={styles.faq}>
       <div className="container py-5 px-[15px] xs:py-large xs:px-5 sm:p-large md:py-x-large xl:px-x-large bg-lynch-100 rounded-2xl xs:rounded-large">
         <h2 className="mb-space-large xl:mb-x-large">
-          {replaceVariablesInText(data.attributes.title, params)}
+          {/* {replaceVariablesInText(data.attributes.title, params)} */}
+          {data.attributes.title}
         </h2>
         <div className={styles.list}>
-          {data.attributes.faqs.map((item) => (
+          {faqsList.map((item) => (
             <FAQItem key={item.id} item={item} params={params} />
           ))}
         </div>
+        {faqsList.length !== faqs.length && (
+          <Button
+            style={"outlined-full"}
+            icon={"arrow-down"}
+            onClick={() => setFaqsList(faqs)}
+            className={"mt-5 sm:mt-space-large xl:mt-x-large"}
+          >
+            Показать все
+          </Button>
+        )}
       </div>
     </section>
   );
--- src/components/faq/faq-item/FaqItem.jsx
+++ src/components/faq/faq-item/FaqItem.jsx
@@ -1,28 +1,15 @@
-"use client";
-
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Icon from "@/components/ui/icon/Icon";
-import { replaceVariablesInText } from "@/utils/extractDataFromParams";
 
-export default function FAQItem({ item, params }) {
+export default function FAQItem({ item }) {
+  console.log("item: ", item);
   const [open, setOpen] = useState(false);
-  const [question, setQuestion] = useState(item.question);
-  const [answer, setAnswer] = useState(item.answer);
-
-  useEffect(() => {
-    const updateText = async () => {
-      setQuestion(replaceVariablesInText(item.question, params));
-      setAnswer(replaceVariablesInText(item.answer, params));
-    };
-    updateText();
-  }, [item.question, item.answer, params]);
+  console.log("open: ", open);
 
   return (
     <div className="mb-3.5 cursor-pointer" onClick={() => setOpen(!open)}>
       <div className="flex items-center py-2.5 px-[15px] md:py-[15px] md:px-large xl:py-4 bg-white rounded-2xl xs:rounded-large">
-        <h4 className="flex-auto">
-          {question}
-        </h4>
+        <h4 className="flex-auto">{item.question}</h4>
         <div className="flex-0 py-1.5 px-2.5 md:py-2.5 md:px-5 xl:py-2 xl:px-7 xl:basis-[11px] bg-navy-100 rounded-large xl:rounded-x-large">
           <Icon
             name={"arrow-left"}
@@ -40,7 +27,7 @@ export default function FAQItem({ item, params }) {
           "opacity-100 h-auto py-2.5 px-[15px] xs:px-5 sm:py-[15px] sm:px-large !z-10"
         }`}
       >
-        {answer}
+        {item.answer}
       </p>
     </div>
   );
